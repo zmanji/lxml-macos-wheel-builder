12c62aabdf657749c20f6d5efb4597005a7ac676
Zameer Manji <zmanji@gmail.com>
2023-04-24 18:30:10 -0400

    add basic gh actions yaml

--- build.bash
+++ build.bash
@@ -2,4 +2,19 @@
 
 set -euox pipefail
 
+python3.9 --help
+
+python3.10 --help
+
+python3.11 --help
+
+python3.12 --help
+
+sudo xcode-select -s /Applications/Xcode_14.3.app/Contents/Developer
+
+xcrun --sdk macosx --show-sdk-path
+xcrun --sdk macosx --show-sdk-platform-path
+xcrun --sdk macosx --show-sdk-version
+xcrun --sdk macosx --show-sdk-build-version
+
 exit 1
--- setup-pythons.bash
+++ setup-pythons.bash
@@ -3,7 +3,7 @@
 set -euox pipefail
 
 wget https://www.python.org/ftp/python/3.12.0/python-3.12.0a7-macos11.pkg
-sudo installer -pkg python-3.12.0a7-macos11 -target /
+sudo installer -pkg python-3.12.0a7-macos11.pkg -target /
 rm *.pkg
 
 wget https://www.python.org/ftp/python/3.11.3/python-3.11.3-macos11.pkg
@@ -11,7 +11,7 @@ sudo installer -pkg python-3.11.3-macos11.pkg -target /
 rm *.pkg
 
 wget https://www.python.org/ftp/python/3.10.11/python-3.10.11-macos11.pkg
-sudo installer -pkg ./python3.10.11.macos11.pkg -target /
+sudo installer -pkg python3.10.11.macos11.pkg -target /
 rm *.pkg
 
 wget https://www.python.org/ftp/python/3.9.13/python-3.9.13-macos11.pkg
