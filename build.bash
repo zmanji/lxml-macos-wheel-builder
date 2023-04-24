2c50d212fbabe47b9f40d351e44f3d3ce1ec4778
Zameer Manji <zmanji@gmail.com>
2023-04-24 19:08:59 -0400

    simplify build.bash

--- build.bash
+++ build.bash
@@ -2,20 +2,14 @@
 
 set -euox pipefail
 
-python3.9 --help
-
-python3.10 --help
-
-python3.11 --help
-
-python3.12 --help
-
 sudo xcode-select -s /Applications/Xcode_14.3.app/Contents/Developer
 
 xcrun --sdk macosx --show-sdk-path
 xcrun --sdk macosx --show-sdk-platform-path
 xcrun --sdk macosx --show-sdk-version
 xcrun --sdk macosx --show-sdk-build-version
+sw_vers
+arch
 
 wget https://github.com/zmanji/reproducible-wheel-builder/releases/download/v0.0.1/main.pex
 chmod +x main.pex
