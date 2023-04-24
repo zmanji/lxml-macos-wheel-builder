d97b14e726ccce2de01150f2508f775c696a92ad
Zameer Manji <zmanji@gmail.com>
2023-04-24 18:48:46 -0400

    use globs

--- setup-pythons.bash
+++ setup-pythons.bash
@@ -3,17 +3,17 @@
 set -euox pipefail
 
 wget https://www.python.org/ftp/python/3.12.0/python-3.12.0a7-macos11.pkg
-sudo installer -pkg python-3.12.0a7-macos11.pkg -target /
+sudo installer -dumplog -pkg *.pkg -target /
 rm *.pkg
 
 wget https://www.python.org/ftp/python/3.11.3/python-3.11.3-macos11.pkg
-sudo installer -pkg python-3.11.3-macos11.pkg -target /
+sudo installer -dumplog -pkg *.pkg -target /
 rm *.pkg
 
 wget https://www.python.org/ftp/python/3.10.11/python-3.10.11-macos11.pkg
-sudo installer -pkg python3.10.11.macos11.pkg -target /
+sudo installer -dumplog -pkg *.pkg -target /
 rm *.pkg
 
 wget https://www.python.org/ftp/python/3.9.13/python-3.9.13-macos11.pkg
-sudo installer -pkg python-3.9.13-macos11.pkg -target /
+sudo installer -dumplog -pkg *.pkg -target /
 rm *.pkg
