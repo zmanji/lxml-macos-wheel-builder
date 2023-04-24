938ee3efb18bcdb49de43deef82736a5eff8f6ce
Zameer Manji <zmanji@gmail.com>
2023-04-24 19:01:04 -0400

    add build logic

--- build.bash
+++ build.bash
@@ -17,4 +17,12 @@ xcrun --sdk macosx --show-sdk-platform-path
 xcrun --sdk macosx --show-sdk-version
 xcrun --sdk macosx --show-sdk-build-version
 
-exit 1
+wget https://github.com/zmanji/reproducible-wheel-builder/releases/download/v0.0.1/main.pex
+chmod +x main.pex
+
+
+wget https://files.pythonhosted.org/packages/06/5a/e11cad7b79f2cf3dd2ff8f81fa8ca667e7591d3d8451768589996b65dec1/lxml-4.9.2.tar.gz
+tar xvzf lxml-4.9.2.tar.gz
+
+PEX_PYTHON=$(which python3.9) CFLAGS='-mmacosx-version-min=11' SOURCE_DATE_EPOCH=0 \
+  ./main.pex --lock setuptools.lock --src ./lxml-4.9.2 --out ./out --dist wheel
