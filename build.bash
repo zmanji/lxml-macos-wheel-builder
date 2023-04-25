fec9be471cfa428ccbb6cfb0fb84df364a2f5856
Zameer Manji <zmanji@gmail.com>
2023-04-24 22:52:44 -0400

    debug

--- build.bash
+++ build.bash
@@ -18,7 +18,8 @@ chmod +x main.pex
 wget https://files.pythonhosted.org/packages/06/5a/e11cad7b79f2cf3dd2ff8f81fa8ca667e7591d3d8451768589996b65dec1/lxml-4.9.2.tar.gz
 tar xvzf lxml-4.9.2.tar.gz
 
-for py in python3.9 python3.10 python3.11
+# for py in python3.9 python3.10 python3.11
+for py in python3.10
 do
   MACOSX_DEPLOYMENT_TARGET=11.0 PEX_PYTHON=$(which $py) SOURCE_DATE_EPOCH=0 \
     ./main.pex --lock setuptools.lock --src ./lxml-4.9.2 --out ./out --dist wheel
--- make-index.py
+++ make-index.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.11
 
+import sys
 import os
 import shlex
 import subprocess
@@ -7,7 +8,12 @@ import subprocess
 def main():
     newtag = os.environ["NEW_TAG"]
     cmd = f"gh release  view {newtag} --jq '.assets[].url' --json assets"
-    output = subprocess.run(shlex.split(cmd), capture_output=True, check=True, text=True).stdout.splitlines()
+    try:
+        output = subprocess.run(shlex.split(cmd), capture_output=True, check=True, text=True).stdout.splitlines()
+    except subprocess.CalledProcessError as e:
+        print(e.stderr, file=sys.stderr)
+        print(e.stdout, file=sys.stderr)
+        sys.exit(1)
 
     print("<html>")
     for o in output:
