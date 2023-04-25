98919958cba4f4814b81dd9e0f6bddd7f32ba531
Zameer Manji <zmanji@gmail.com>
2023-04-24 23:22:27 -0400

    fix index upload command

--- make-index.py
+++ make-index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.11
 
+from pathlib import Path
 import sys
 import os
 import shlex
@@ -7,6 +8,7 @@ import subprocess
 
 def main():
     newtag = os.environ["NEW_TAG"]
+    repo = os.environ["REPO"]
     cmd = f"gh release  view {newtag} --jq '.assets[].url' --json assets"
     try:
         output = subprocess.run(shlex.split(cmd), capture_output=True, check=True, text=True).stdout.splitlines()
@@ -15,12 +17,27 @@ def main():
         print(e.stdout, file=sys.stderr)
         sys.exit(1)
 
-    print("<html>")
+    index = ""
+    index += "<html>\n"
     for o in output:
-        print("<a>")
-        print(o)
-        print("</a>")
-    print("</html>")
+        index += "<a>\n"
+        index += o
+        index += "\n"
+        index += "</a>"
+    index += "</html>\n"
+
+    
+    Path("./index.html").write_text(index)
+
+     
+    cmd = f"gh release upload {newtag} index.html -R {repo}"
+    try:
+        output = subprocess.run(shlex.split(cmd), capture_output=True, check=True, text=True).stdout.splitlines()
+    except subprocess.CalledProcessError as e:
+        print(e.stderr, file=sys.stderr)
+        print(e.stdout, file=sys.stderr)
+        sys.exit(1)
+    print(output)
 
 if __name__ == '__main__':
     main()
