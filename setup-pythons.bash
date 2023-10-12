9ed579b3b9a9eafa1b160e2b4edbdc2e53cb763a
Zameer Manji <zmanji@gmail.com>
2023-10-12 00:28:15 -0400

    drop support for python3.9

--- setup-pythons.bash
+++ setup-pythons.bash
@@ -13,7 +13,3 @@ rm *.pkg
 wget https://www.python.org/ftp/python/3.10.11/python-3.10.11-macos11.pkg
 sudo installer -dumplog -pkg *.pkg -target /
 rm *.pkg
-
-wget https://www.python.org/ftp/python/3.9.13/python-3.9.13-macos11.pkg
-sudo installer -dumplog -pkg *.pkg -target /
-rm *.pkg
